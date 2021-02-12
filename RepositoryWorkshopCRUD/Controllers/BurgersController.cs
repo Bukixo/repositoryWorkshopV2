58e49dd08f8e2d05534e737397a16a44fc50fea2
Buki Thompson <bukola.thompson@principleone.co.uk>
2021-02-12 18:02:25 +0000

    crud app created

---
+++ RepositoryWorkshopCRUD/Controllers/BurgersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RepositoryWorkshopCRUD.Models;
+
+namespace RepositoryWorkshopCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BurgersController : ControllerBase
+    {
+        private readonly BurgerContext _context;
+
+        public BurgersController(BurgerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Burgers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Burger>>> GetBurgers()
+        {
+            //return await _context.Burgers.ToListAsync(); without the DTO model
+            return await _context.Burgers
+                .Select(burgerInstance => burgerInstance)
+                .ToListAsync();
+        }
+
+        // GET: api/Burgers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Burger>> GetBurger(long id)
+        {
+            var burger = await _context.Burgers.FindAsync(id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+
+            return burger;
+        }
+
+        //PUT: api/Burgers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBurger(long id, Burger burger)
+        {
+            if (id != burger.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(burger).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BurgerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Burgers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Burger>> PostBurger(Burger burger)
+        {
+            _context.Burgers.Add(burger);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBurgers), new { id = burger.Id });
+        }
+
+        // DELETE: api/Burgers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBurger(long id)
+        {
+            var burger = await _context.Burgers.FindAsync(id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+
+            _context.Burgers.Remove(burger);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BurgerExists(long id)
+        {
+            return _context.Burgers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RepositoryWorkshopCRUD/Models/Burger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryWorkshopCRUD.Models
+{
+    public class Burger
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
