c9a07f7300b91d3df9d5179171f3592ab86810f7
Buki Thompson <bukola.thompson@principleone.co.uk>
2021-02-12 18:21:50 +0000

    added the repository interface

---
+++ RepositoryWorkshopCRUD/Repository/IBurgerRepository.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryWorkshopCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryWorkshopCRUD.Repository
+{
+    interface IBurgerRepository
+    {
+        Task<List<Burger>> ListAllBurgers();
+        Task<Burger> GetBurgerByID(int id);
+        Task<Burger> InsertBurger(Burger burger);
+        Task<Burger> DeleteBurger(int burgerID);
+        Task<Burger> UpdateBurger(Burger burger);
+        Task<Burger> Save();
+    }
+}
